761dc4003efc43173f2542871508c28926e70478
Yair-Rios <vvrhz11@gmail.com>
2023-06-05 12:42:11 -0600

    organizacion de archivos

--- Ejercicios de practica/datosDuplicados.js
+++ Ejercicios de practica/datosDuplicados.js
@@ -1,7 +1,12 @@
 /* Buscar duplicados: Escribir una función que encuentre los elementos duplicados en un
 array y los devuelva en otro array */
 function datosDuplicados(array){
-  
+  let repetidos=[]
+  let resultado=[]
+  array.map(items => {
+    if(repetidos.includes(items))
+    repetidos.push(items)
+  })
   return duplicados
 }
 console.log(datosDuplicados(["jose","luis","luis","jose","marlon","yair","marlon"]));
