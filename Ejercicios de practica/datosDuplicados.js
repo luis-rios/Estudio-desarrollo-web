a16b5f6479056d2f98eee1b2a1de3ec0a4e5fe84
Yair-Rios <vvrhz11@gmail.com>
2023-06-05 13:45:04 -0600

    agregando ejercicio datos duplicados

--- Ejercicios de practica/datosDuplicados.js
+++ Ejercicios de practica/datosDuplicados.js
@@ -1,12 +1,12 @@
 /* Buscar duplicados: Escribir una función que encuentre los elementos duplicados en un
 array y los devuelva en otro array */
 function datosDuplicados(array){
-  let repetidos=[]
-  let resultado=[]
-  array.map(items => {
-    if(repetidos.includes(items))
-    repetidos.push(items)
-  })
+  let duplicados=[]
+  array.forEach(function(elemento,index,array){
+    if(array.indexOf(elemento)!=index){
+      duplicados.push(elemento)
+    }
+  });
   return duplicados
 }
 console.log(datosDuplicados(["jose","luis","luis","jose","marlon","yair","marlon"]));
