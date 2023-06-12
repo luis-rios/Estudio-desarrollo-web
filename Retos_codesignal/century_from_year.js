29edea1f8fc2c6c269dc5dc1cf161724aff4a390
Yair-Rios <vvrhz11@gmail.com>
2023-06-12 11:41:09 -0600

    agregando ejercicios

--- Retos_codesignal/century_from_year.js
+++ Retos_codesignal/century_from_year.js
@@ -1,2 +1,4 @@
+/* Dado un año retorna el cilgo en el que está. El primer ciglo abarca desde  el año 1
+hasta he incluyendo el año 100, el segundo desde el año 101 incluyendo el año 200 etc. */
 const CenturyFromYear=(year) => {return Math.ceil(year/100 )}
 console.log(CenturyFromYear(1700));
