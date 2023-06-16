bf5e1cac8b7e93dccb16f9dea38846a5dc08de47
Yair-Rios <vvrhz11@gmail.com>
2023-06-15 21:22:47 -0600

    agregando cambios a funcion

--- Retos_codesignal/secuenciaMasLarga.js
+++ Retos_codesignal/secuenciaMasLarga.js
@@ -1,27 +1,18 @@
-const secuencia=[3, 5, 67, 98, 3]
+/* const secuencia=[3, 5, 67, 98, 3] */
+/* const secuencia=[1,2,3,2,5] */
+const secuencia=[1,3,2,1]
+/* const secuencia=[8,1,2,3] */
 const secuenciaMasLarga=(array)=>{
 	let contador=0;
-	let siclos=0
-	while (siclos<=array.length-2){
-		if(array[siclos]<array[siclos+1]){			
-			siclos++
-		}else if(siclos<array.length-2&&array[siclos]<array[siclos+2]){
-			array.splice(siclos+1,1)
-			contador+=1
-			siclos=0
-		}else if (siclos===array.length-2&&array[siclos]>=array[siclos+1]){
-			array.splice(siclos+1,1)
-			contador+=1
-			siclos=0
-		}else{
-			array.splice(siclos,1)
-			contador+=1
-			siclos=0
-		}
-		if(contador>1||array.length<3){
-			break
+	let siclo=0;
+	while (siclo<array.length-2){
+
+		if()
+		if(array[siclo]>=array[siclo+1]){
+			array.splice(siclo,1)
 		}
 	}
+	
 	return contador<2?true:false
 }
 console.log(secuenciaMasLarga(secuencia));
