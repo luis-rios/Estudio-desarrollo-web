c17bcca0e76336fe313d3aa7a921389e27a834d6
Yair-Rios <vvrhz11@gmail.com>
2023-06-16 15:17:20 -0600

    agregando archivos de pruebas

---
+++ Retos_codesignal/pruebas.js
@@ -0,0 +1,24 @@
+const numbers=[8,1,2,5,2];
+const Secuencia=(array=>{
+  let contador=0;
+  let siclos=0;
+  while(siclos<array.length&&contador<2){
+     if(array[siclos]>=array[siclos+1]&&array[siclos+2]<=array[siclos]){
+      array.splice(siclos,1)
+      contador++
+      siclos-=1
+    }else if(array[siclos+2]==null&&array[siclos+1]<=array[siclos]&&array[siclos+2]>=array[siclos]){
+      array.splice(siclos+1,1)
+      contador++
+      siclos-=1
+    }
+    else if(array[siclos]>=array[siclos+1]){
+      array.splice(siclos+1,1)
+      contador++
+      siclos-=1
+    }else{siclos++}
+  }
+  return contador<2?true:false
+})
+  
+console.log(Secuencia(numbers))
