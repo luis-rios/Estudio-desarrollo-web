b9d9056f808639d78812144b4b286f3abc94d52c
Yair-Rios <vvrhz11@gmail.com>
2023-06-19 23:07:09 -0600

    agregando cambios

---
+++ Retos_codesignal/caractes_en_comun.js
@@ -0,0 +1,13 @@
+const solution = (st1, st2) => {
+  let array1 = st1.split("");
+  let array2 = st2.split("");
+  let contador = 0;
+  array2.forEach(item => {
+    if (array1.includes(item)) {
+      contador++;
+    }
+  });
+
+  return contador;
+};
+console.log(solution("aabcc", "adcaa"));
--- Retos_codesignal/fizzBuzz.js
+++ Retos_codesignal/fizzBuzz.js
@@ -6,11 +6,11 @@ const fizzBuzz=()=>{
   let response=[]
   for(let i=1;i<=100;i++){
     if(i%3===0&&i%5===0){
-      console.log("FizzBuzz")
+      console.log(i,": FizzBuzz")
     }else if(i%3===0){
-      console.log("Fizz")
+      console.log(i,": Fizz")
     }else if(i%5===0){
-      console.log("Buzz")
+      console.log(i,": Buzz")
     }
   }
   
