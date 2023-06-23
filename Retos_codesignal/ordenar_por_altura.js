71e54765ba41bdef9263c72085684b60ed627670
Yair-Rios <vvrhz11@gmail.com>
2023-06-22 20:17:43 -0600

    agregando ejercicio

---
+++ Retos_codesignal/ordenar_por_altura.js
@@ -0,0 +1,17 @@
+const Respuesta=(array)=>{
+  let copia = [...array]
+  let response=[]
+  let contador=0;
+  copia = copia.filter(item =>{return item!=-1}).sort((a,b)=>{return a-b})
+  array.forEach((item,index)=>{
+    if(item!=-1){
+      response.push(copia[contador])
+      contador+=1
+    }else {
+      response.push(item)
+    }
+  })
+  return response
+  
+}
+console.log(Respuesta([4, 2, 9, 11, 2, 16]));
