bd6a9f3864293ad614a4be3ef879012693528631
Yair-Rios <vvrhz11@gmail.com>
2023-06-26 23:46:30 -0600

    agregando ejercicio

---
+++ Retos_codesignal/son_igual_fuertes.js
@@ -0,0 +1,13 @@
+const solution = (yl,yr,fl,fr)=>{
+  let me=[]
+  let fri=[]
+  me  [0] = (Math.max(yl,yr))
+  me  [1] = (Math.min(yl,yr))
+  fri [0] = (Math.max(fr,fl))
+  fri [1] = (Math.min(fr,fl))
+  me = me.map(item => {return item.toString()})
+  fri = fri.map(item =>{return item.toString()})
+  console.log(me,fri);
+  return me.join("") == fri.join("")   
+}
+console.log(solution(10,14,14,10));
