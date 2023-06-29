f1b5aa2bc83faa53190c1aee4d898ed967f59378
Yair-Rios <vvrhz11@gmail.com>
2023-06-28 20:43:35 -0600

    agregando nuevo ejercicio

--- Retos_codesignal/es_direccion_ipv.js
+++ Retos_codesignal/es_direccion_ipv.js
@@ -4,13 +4,16 @@
   return newArray.map(item =>{return item>255?false:true}).includes(false)?false:true 
 }
 console.log(solution("172.16.254.1")); */
-const solution=(string)=>{
+/* const solution=(string)=>{
   let newArray =string.split(".").map(item => {return item==""||isNaN(item) || item>255?
     false: parseInt(item)})
   console.log(newArray.length);
   console.log(newArray);
   return newArray.length<4||newArray.includes(false) ? false:true
-  
-  
+   
 }
-console.log(solution("1"));
+console.log(solution("1")); */
+const solution =(string)=>{
+  const expresion = /^ $/
+}
+console.log(solution(".1.192.255"));
